be851ed49c3586728ec4377809330136371084d5
Gurrala Sai Haneesh <145466482+GURRALASAIHANEESH@users.noreply.github.com>
2025-06-15 12:02:12 +0530

    app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -11,7 +11,7 @@ user_input = st.text_input("Enter a task and see how the agent plans, refines, s
 if st.button("Run Agent Workflow"):
     if user_input.strip():
         with st.spinner("Thinking..."):
-            result = graph.invoke({"input": user_input, "results": [], "subtasks": [], "retry_count": 0, "done": False})
+            result = graph.invoke({"input": user_input, "results": [], "subtasks": [], "retry_count": 0, "done": False},config={"recursion_limit": 10})
             st.subheader("✅ Final Output:")
             for step in result["results"]:
                 st.markdown(f"- {step}")
